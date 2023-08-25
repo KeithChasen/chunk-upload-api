09b2d48deff0f2142d2d09c75c27efb487b2a8e7
keithchasen <keithchasen89@gmail.com>
2023-08-25 16:13:18 +0200

    adding chunks and save image

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+import md5 from 'md5';
 
 const app = express();
 app.use(
@@ -16,10 +18,32 @@ app.get('/', (req, res) => {
 })
 
 app.post('/upload', (req, res) => {
-    const { name, size, currentChunkIndex, totalChunks } = req.query;
+    const { name, currentChunkIndex, totalChunks } = req.query;
+    
+    const firstChunk = parseIncurrentChunkIndex === 0;
+    const lastChunk = parseInt(currentChunkIndex) === parseInt(totalChunks) - 1
+    
     const ext = name.split('.').pop(); // test.jpg => jpg
 
-    console.log( req.body );
+    // remove this part: data:application/octet-stream;base64
+    const data = req.body.toString().split(',')[1];
+
+    const buffer = new Buffer.from(data);
+
+    const tmpFileName = `tmp_${md5(name)}.${ext}`;
+
+    if (firstChunk) {
+        fs.unlinkSync(`./uploads/${tmpFileName}`)
+    }
+    
+    fs.appendFileSync(`./uploads/${tmpFileName}`, buffer);
+
+    if (lastChunk) {
+        const finalFileName = md5(Date.now()).substring(0,6) + '.' + ext;
+        fs.renameSync(`./uploads/${tmpFileName}`, `./uploads/${finalFileName}`);
+
+    }
+
 
     res.send('ok');
 });
