29857d7624674009800c47140d6b5e2d89672b68
keithchasen <keithchasen89@gmail.com>
2023-08-25 11:08:11 +0200

    collecting params from request

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(
+    bodyParser.raw({ 
+        type: 'application/octet-stream',
+        limit: '100mb'
+    })
+);
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('ok');
+})
 
 app.post('/upload', (req, res) => {
-    
+    const { name, size, currentChunkIndex, totalChunks } = req.query;
+    const ext = name.split('.').pop(); // test.jpg => jpg
+
+    console.log( req.body );
+
+    res.send('ok');
 });
 
 app.listen(4000);
